779b5168c6a2ad4954bf4b318658cbde7b264a81
kamilah santos <kahsantos89@gmail.com>
2020-09-20 17:48:01 -0300

    add postgree

--- src/main/java/com/zenvia/newhorizon/agendabot/controller/UserController.java
+++ src/main/java/com/zenvia/newhorizon/agendabot/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/api/v1")
+@RequestMapping("/v1")
 public class UserController {
 
   @Autowired
